7084ef45bebc15a62411a8a3dee458f7cee6a6a5
Jomsang <af7078@naver.com>
2024-07-28 23:02:57 +0900

    헤더, 푸터 완료

--- src/views/common/Footer.jsx
+++ src/views/common/Footer.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import styles from 'views/common/Footer.module.css';
 
-const Footer = ({onLogOut}) => {
+
+const Footer = () => {
   return (
-    <div>
-      <p onClick={onLogOut}>임시 로그아웃(클릭하면 로그아웃)</p>
-    </div>
+    <footer>
+      <div className={styles.footerBottom}>
+        <div className={styles.footerBottomLeft}>
+          <select>
+            <option value="en">English</option>
+            <option value="ko">한국어</option>
+            {/* 다른 언어 옵션 추가 가능 */}
+          </select>
+        </div>
+        <div>
+          <span>© 2022 Brand, Inc. • Privacy • Terms • Sitemap</span>
+        </div>
+        <div className={styles.footerBottomRight}>
+          <a href="#"><i className="fab fa-facebook-f"></i></a>
+          <a href="#"><i className="fab fa-twitter"></i></a>
+          <a href="#"><i className="fab fa-linkedin-in"></i></a>
+          <a href="#"><i className="fab fa-youtube"></i></a>
+        </div>
+      </div>
+    </footer>
   );
 };
 
 export default Footer;
+
+
--- src/views/common/Header.jsx
+++ src/views/common/Header.jsx
@@ -1,29 +1,30 @@
 import React from 'react';
 import styles from './Header.module.css';
 
+
 const Header = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.logo}>
-        <img src="path/to/logo.png" alt="Logo" />
-        <span>Shopping</span>
-      </div>
-      <div className={styles.searchBar}>
-        <input type="text" placeholder="Search" />
-        <button>
-          <img src="path/to/search-icon.png" alt="Search" />
-        </button>
-        <button>
-          <img src="path/to/camera-icon.png" alt="Camera" />
-        </button>
-      </div>
-      <div className={styles.icons}>
-        <img src="path/to/cart-icon.png" alt="Cart" />
-        <div className={styles.userProfile}>
-          <img src="path/to/user-icon.png" alt="User Profile" />
-        </div>
+    <header className={styles.container}>
+    <div className={styles.logo}>
+      <img src="/image/shopping.JPG" alt="Logo" />
+      <span>Shopping</span>
+    </div>
+    <div className={styles.searchBar}>
+      <input type="text" placeholder="Search" className={styles.searchInput} />
+      <button className={styles.searchButton}>
+        <img src="/image/search.JPG" alt="Search" />
+      </button>
+      <button className={styles.cameraButton}>
+        <img src="/image/camera.JPG" alt="Search" />
+      </button>
+    </div>
+    <div className={styles.icons}>
+      <img src="/image/bucket.JPG" alt="Cart" />
+      <div className={styles.userProfile}>
+        <img src="/image/user.JPG" alt="User Profile" />
       </div>
     </div>
+    </header>
   );
 };
 
