0f4b0fca65500feda6cee4e0bb15b90f75680ea7
Livart <>
2024-08-02 17:15:51 +0900

    모델디테일생성, 앱라우터 수정

---
+++ src/views/modelDetail/modelDetail.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+function ModelDetail(){
+
+    return(
+        <div className="product-detail">
+      {/* <Breadcrumb /> */}
+      <div className="product-content">
+        {/* <ModelImages />
+        <ModelInfo /> */}
+        <h3>Model Detail Page</h3>
+      </div>
+    </div>
+    );
+}
+
+export default ModelDetail;
