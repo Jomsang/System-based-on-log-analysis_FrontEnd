13b28243611f3e156d2fa13e2afbb4f1d9966da6
HyoBeomLee <llhbum@naver.com>
2024-08-08 10:47:00 +0900

    모델디테일 기본 구성 + css

--- src/views/modelDetail/modelDetail.jsx
+++ src/views/modelDetail/modelDetail.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
+import Breadcrumb from './BreadCrumb';
+import ModelImages from './ModelImages';
+import ModelInfo from './ModelInfo';
+import styles from './ModelDetail.module.css'; 
 
-
-function ModelDetail(){
-
-    return(
-        <div className="product-detail">
-      {/* <Breadcrumb /> */}
-      <div className="product-content">
-        {/* <ModelImages />
-        <ModelInfo /> */}
-        <h3>Model Detail Page</h3>
+const ModelDetail = () => {
+  return (
+    <div className={styles.modelDetail}>
+      {/* <h3>model Detail page</h3> */}
+      <Breadcrumb />
+      <div className={styles.modelContent}>
+        <ModelImages />
+        <ModelInfo />
       </div>
     </div>
-    );
+  );
 }
 
 export default ModelDetail;
